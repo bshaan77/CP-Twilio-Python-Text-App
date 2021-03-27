43ed66f720b22c9cd321185aa7c6131c6f2675e6
Ishaan Bansal <ishaanbansal1001@gmail.com>
2021-03-26 23:53:53 -0700

    add sid

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
+account_sid = "AC22bf26fe3fb7f01734943b0a1789e72e"
 auth_token = "Your Auth Token"
 my_cell = "+Your Cell Number"
 my_twilio = "+Your Twilio Number"
