b5310f13f3befa06f50a9cddc2a6231c5e5763a4
Ishaan Bansal <ishaanbansal1001@gmail.com>
2021-03-26 23:54:40 -0700

    add credentials

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = "AC22bf26fe3fb7f01734943b0a1789e72e"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+auth_token = "e777d9c3c67ae6fac5f20d1a2a34e8f9"
+my_cell = "+19259643840"
+my_twilio = "+18607173110"
